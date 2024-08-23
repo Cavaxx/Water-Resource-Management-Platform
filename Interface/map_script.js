5745b5243322beb63fda49b3d771057d37b695ea
pariamelle <mariaamalia.pelle@studenti.unitn.it>
2024-08-23 17:30:01 +0200

    Map added, markers in the making

--- Interface/map_script.js
+++ Interface/map_script.js
@@ -6,8 +6,27 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
+L.marker([46.147744, 11.197343]).addTo(map).bindPopup('Test Marker');
 // Aggiungere un marker a titolo di esempio
-L.marker([46.1, 11.1]).addTo(map)
-    .bindPopup('Trento')
-    .openPopup();
+Papa.parse('../data/water_facilities_trentino.csv', {
+    download: true,
+    header: true,
+    complete: function(results) {
+        results.data.forEach(function(facility) {
+            // Crea un marker per ogni sito
+            var marker = L.marker([facility.latitude, facility.longitude]).addTo(map);
+            
+            // Crea il contenuto del popup con le informazioni desiderate
+            var popupContent = `
+                <b>Type:</b> ${facility.descrizione}
+                <b>ID Sito:</b> ${facility.id_sito}<br>
+                <b>Recettore:</b> ${facility.recettore}<br>
+                <b>Comuni Serviti:</b> ${facility.comuni_serviti}
+            `;
+
+            // Aggiungi il popup al marker, senza aprirlo automaticamente
+            marker.bindPopup(popupContent);
+        });
+    }
+});
 
